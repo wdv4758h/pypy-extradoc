0c3ed96d2369cd802fea5be2b67d784623ec4d05
Carl Friedrich Bolz <cfbolz@gmx.de>
2009-06-09 14:24:18 +0000

    Simplify hints a bit, and add a footnote that it is different in reality. They confused reviewers quite a bit.

--- talk/icooolps2009/code/tlr-paper-full.py
+++ talk/icooolps2009/code/tlr-paper-full.py
@@ -7,9 +7,7 @@ def interpret(bytecode, a):
     regs = [0] * 256
     pc = 0
     while True:
-        tlrjitdriver.jit_merge_point(
-            bytecode=bytecode, pc=pc,
-            a=a, regs=regs)
+        tlrjitdriver.jit_merge_point()
         opcode = ord(bytecode[pc])
         pc += 1
         if opcode == JUMP_IF_A:
@@ -17,9 +15,7 @@ def interpret(bytecode, a):
             pc += 1
             if a:
                 if target < pc:
-                    tlrjitdriver.can_enter_jit(
-                        bytecode=bytecode, pc=target,
-                        a=a, regs=regs)
+                    tlrjitdriver.can_enter_jit()
                 pc = target
         elif opcode == MOV_A_R:
             ... # rest unmodified
