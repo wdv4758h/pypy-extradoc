53a3645d49fa8d1d075896a7f802cc3708562bbc
Hakan Ardo <hakan@debian.org>
2011-06-09 07:46:48 +0200

    different results for different array sizes. cache effects?

--- talk/iwtc11/benchmarks/convolution/conv3.c
+++ talk/iwtc11/benchmarks/convolution/conv3.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 
 #define N 100000000
 double a[N], b[N-2];
 
-//void conv(double *a, double *k, double *b) {
-void conv(double *__restrict__ a, double *__restrict__ k, double *__restrict__ b) {
+void conv(double *a, double *k, double *b, int n) {
+//void conv(double *__restrict__ a, double *__restrict__ k, double *__restrict__ b, int n) {
   int i;
-  for (i=0; i<N-2; i++) {
+  for (i=0; i<n-2; i++) {
     b[i] = k[2]*a[i] + k[1]*a[i+1] + k[0]*a[i+2];
   }
 }
 
-int main() {
+int main(int ac, char **av) {
   double k[3] = {-1, 0, 1};
-    int i;
-    for (i=0; i<N; i++) a[i] = 1;
-    conv(a,k, b);
-    printf("%f\n", b[N/2]);
-    fprintf(stderr, "conv3:         ");
-    return 0;
+  int i;
+  for (i=0; i<N; i++) a[i] = 1;
+  int n = atoi(av[1]);
+  for (i=0; i<n; i++)
+    conv(a,k, b, N/n);
+  printf("%f\n", b[N/2]);
+  fprintf(stderr, "conv3(1e%d):     ", ((int) log10(N/n)));
+  return 0;
 }
