dab760f2fa89c1d90f577a01f0674da88cbe08c4
Maciej Fijalkowski <fijall@gmail.com>
2011-06-16 21:48:32 +0200

    start describing benchmarks

--- talk/iwtc11/benchmarks/benchmark.sh
+++ talk/iwtc11/benchmarks/benchmark.sh
@@ -5,7 +5,7 @@ echo $*
 if [ "$1" == "gcc" ]; then
     ./runner.py -n 5 -c "$*" sqrt/sqrt_double.c
     ./runner.py -n 5 -c "$*" sqrt/sqrt_long.c
-    #./runner.py -n 5 -c "$*" sqrt/sqrt_fix16.c
+    ./runner.py -n 5 -c "$*" sqrt/sqrt_fix16.c
     #./runner.py -n 5 -c "$* -lm" convolution/conv3.c 1
     #./runner.py -n 5 -c "$* -lm" convolution/conv5.c 1
     ./runner.py -n 5 -c "$* -lm" convolution/conv3.c 100
@@ -20,7 +20,7 @@ if [ "$1" == "gcc" ]; then
 else
     $* ./runner.py -n 10 sqrt/sqrt.py main int
     $* ./runner.py -n 10 sqrt/sqrt.py main float
-    #$* ./runner.py -n 10 sqrt/sqrt.py main Fix16
+    $* ./runner.py -n 10 sqrt/sqrt.py main Fix16
     #$* ./runner.py -n 10 convolution/convolution.py conv3 1
     #$* ./runner.py -n 10 convolution/convolution.py conv5 1
     $* ./runner.py -n 10 convolution/convolution.py conv3 100
--- talk/iwtc11/benchmarks/runall.sh
+++ talk/iwtc11/benchmarks/runall.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-#./benchmark.sh pypy
-./benchmark.sh pypy --jit enable_opts=intbounds:rewrite:virtualize:heap:unroll
+./benchmark.sh pypy
+#./benchmark.sh pypy --jit enable_opts=intbounds:rewrite:virtualize:heap:unroll
 ./benchmark.sh pypy --jit enable_opts=intbounds:rewrite:virtualize:heap
 #./benchmark.sh gcc
 ./benchmark.sh gcc -O2
 ./benchmark.sh gcc -O3 -march=native -fno-tree-vectorize
-#./benchmark.sh python2.7
-
+./benchmark.sh python2.7
+./benchmark.sh python2.6 psyco-wrapper.py
