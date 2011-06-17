f7cf7b55efd09bdaf5c539f04e7a1a39838d20d8
Maciej Fijalkowski <fijall@gmail.com>
2011-06-17 11:48:22 +0200

    small improvements, write para about benchmarks

--- talk/iwtc11/benchmarks/benchmark.sh
+++ talk/iwtc11/benchmarks/benchmark.sh
@@ -18,24 +18,30 @@ if [ "$1" == "gcc" ]; then
     ./runner.py -n 5 -c "$* -lstdc++" image/sobel.cc 1002 1002
     rm a.out
 else
-    $* ./runner.py -n 10 sqrt/sqrt.py main int
-    $* ./runner.py -n 10 sqrt/sqrt.py main float
-    $* ./runner.py -n 10 sqrt/sqrt.py main Fix16
-    #$* ./runner.py -n 10 convolution/convolution.py conv3 1
-    #$* ./runner.py -n 10 convolution/convolution.py conv5 1
-    $* ./runner.py -n 10 convolution/convolution.py conv3 100
-    $* ./runner.py -n 10 convolution/convolution.py conv5 100
-    $* ./runner.py -n 10 convolution/convolution.py conv3 1000
-    $* ./runner.py -n 10 convolution/convolution.py conv5 1000
-    $* ./runner.py -n 10 convolution/convolution.py conv3x3 1000000 3
-    $* ./runner.py -n 10 convolution/convolution.py conv3x3 1000 1000
-    $* ./runner.py -n 10 convolution/convolution.py dilate3x3 1000 1000
-    $* ./runner.py -n 10 image/noborder.py main NoBorderImagePadded
-    $* ./runner.py -n 10 image/noborder.py main NoBorderImagePadded iter
-    $* ./runner.py -n 10 image/noborder.py main NoBorderImagePadded range
-    $* ./runner.py -n 10 image/noborder.py main NoBorderImage
-    $* ./runner.py -n 10 image/noborder.py main NoBorderImage iter
-    $* ./runner.py -n 10 image/noborder.py main NoBorderImage range
-    $* ./runner.py -n 10 image/sobel.py main NoBorderImagePadded
-    $* ./runner.py -n 10 image/sobel.py main NoBorderImagePadded uint8
+    if [ "$1" == "python2.7" ]; then
+        EXTRA_OPTS='-w 0 -n 1'
+    fi
+    if [ "$1" == "python2.6" ]; then
+        EXTRA_OPTS='-w 1 -n 1'
+    fi
+    $* ./runner.py $EXTRA_OPTS sqrt/sqrt.py main int
+    $* ./runner.py $EXTRA_OPTS sqrt/sqrt.py main float
+    $* ./runner.py $EXTRA_OPTS sqrt/sqrt.py main Fix16
+    #$* ./runner.py $EXTRA_OPTS convolution/convolution.py conv3 1
+    #$* ./runner.py $EXTRA_OPTS convolution/convolution.py conv5 1
+    $* ./runner.py $EXTRA_OPTS convolution/convolution.py conv3 100
+    $* ./runner.py $EXTRA_OPTS convolution/convolution.py conv5 100
+    $* ./runner.py $EXTRA_OPTS convolution/convolution.py conv3 1000
+    $* ./runner.py $EXTRA_OPTS convolution/convolution.py conv5 1000
+    $* ./runner.py $EXTRA_OPTS convolution/convolution.py conv3x3 1000000 3
+    $* ./runner.py $EXTRA_OPTS convolution/convolution.py conv3x3 1000 1000
+    $* ./runner.py $EXTRA_OPTS convolution/convolution.py dilate3x3 1000 1000
+    $* ./runner.py $EXTRA_OPTS image/noborder.py main NoBorderImagePadded
+    $* ./runner.py $EXTRA_OPTS image/noborder.py main NoBorderImagePadded iter
+    $* ./runner.py $EXTRA_OPTS image/noborder.py main NoBorderImagePadded range
+    $* ./runner.py $EXTRA_OPTS image/noborder.py main NoBorderImage
+    $* ./runner.py $EXTRA_OPTS image/noborder.py main NoBorderImage iter
+    $* ./runner.py $EXTRA_OPTS image/noborder.py main NoBorderImage range
+    $* ./runner.py $EXTRA_OPTS image/sobel.py main NoBorderImagePadded
+    $* ./runner.py $EXTRA_OPTS image/sobel.py main NoBorderImagePadded uint8
 fi
