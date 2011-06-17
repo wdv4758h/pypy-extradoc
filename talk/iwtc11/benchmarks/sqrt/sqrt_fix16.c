511ec402ef364120fa40000cbba59b4100458407
Hakan Ardo <hakan@debian.org>
2011-06-17 09:22:16 +0200

    match changes made to sqrt.py

--- talk/iwtc11/benchmarks/sqrt/sqrt_fix16.c
+++ talk/iwtc11/benchmarks/sqrt/sqrt_fix16.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-  long y = 1234 << 16;
+  long y = 123 << 16;
   long x = y / 2;
   long n = 100000000;
   while (n>0) {
     n -= 1;
-    x = ((x + (y << 16)/x)) / 2;
+    x = ((x + (y << 8)/(x >> 8))) / 2;
   }
   printf("%f\n", ((double) x) / ((double) (1<<16)));
   fprintf(stderr, "sqrt(Fix16):   ");
