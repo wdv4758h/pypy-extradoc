297966a3cb2fba37ed1b5e4f58b7d144abc90655
David Schneider <david.schneider@picle.org>
2012-07-27 15:38:36 +0200

    pick a newer pypy revision that contains all required debug information to run the benchmarks on and collect all jit-* debug messages

--- talk/vmil2012/tool/run_benchmarks.sh
+++ talk/vmil2012/tool/run_benchmarks.sh
@@ -5,11 +5,11 @@ bench_list="${base}/logs/benchs.txt"
 benchmarks="${base}/pypy-benchmarks"
 REV="ff7b35837d0f"
 pypy_co="${base}/pypy"
-PYPYREV='release-1.9'
+PYPYREV='0b77afaafdd0'
 pypy="${pypy_co}/pypy-c"
 pypy_opts=",--jit enable_opts=intbounds:rewrite:virtualize:string:pure:heap:ffi"
 baseline=$(which true)
-logopts='jit-backend-dump,jit-backend-guard-size,jit-log-opt,jit-log-noopt,jit-summary'
+logopts='jit'
 # checkout and build a pypy-c version
 if [ ! -d "${pypy_co}" ]; then
   echo "Cloning pypy repository to ${pypy_co}"
@@ -18,6 +18,8 @@ fi
 #
 cd "${pypy_co}"
 echo "updating pypy to fixed revision ${PYPYREV}"
+hg revert --all
+hg pull -u
 hg update "${PYPYREV}"
 echo "Patching pypy"
 patch -p1 -N < "$base/tool/ll_resume_data_count.patch"
@@ -29,7 +31,6 @@ then
 else
     echo "found!"
 fi
-
 
 # setup a checkout of the pypy benchmarks and update to a fixed revision
 if [ ! -d "${benchmarks}" ]; then
