1fb7613b66491692cad9e2600d8f5ea2e5120281
Maciej Fijalkowski <fijall@gmail.com>
2012-08-03 13:10:05 +0200

    A numpy non-progress report

---
+++ talk/vancouver/run_example1.py
@@ -0,0 +1,21 @@
+from pypy.translator.interactive import Translation
+from pypy.annotation.model import SomeInteger
+
+try:
+    import rlcompleter2
+    rlcompleter2.setup()
+except ImportError:
+    pass
+
+import is_prime
+
+t = Translation(is_prime.is_prime)
+t.annotate([SomeInteger(nonneg=True)])
+t.view()
+t.rtype()
+t.view()
+t.backendopt(backend="c")
+t.view()
+f = t.compile()
+print "value returned by the compiled function with argument 5"
+print f(5)
