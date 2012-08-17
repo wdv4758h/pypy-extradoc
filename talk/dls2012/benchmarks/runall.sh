039f2eb234df91cd1596a9f3950e3dc58ced8eec
Maciej Fijalkowski <fijall@gmail.com>
2012-08-17 12:04:14 +0000

    add lua results

--- talk/dls2012/benchmarks/runall.sh
+++ talk/dls2012/benchmarks/runall.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-./benchmark.sh pypy
-./benchmark.sh pypy --jit enable_opts=intbounds:rewrite:virtualize:string:earlyforce:pure:heap:ffi
+#./benchmark.sh pypy
+#./benchmark.sh pypy --jit enable_opts=intbounds:rewrite:virtualize:string:earlyforce:pure:heap:ffi
 #./benchmark.sh pypy-1.5
 #./benchmark.sh pypy-1.5 --jit enable_opts=intbounds:rewrite:virtualize:heap:unroll
 #./benchmark.sh pypy-1.5 --jit enable_opts=intbounds:rewrite:virtualize:heap
 #./benchmark.sh gcc
 #./benchmark.sh gcc -O2
-./benchmark.sh gcc -O3 -march=native -fno-tree-vectorize
-./benchmark.sh python2.7
-./benchmark.sh python2.6 psyco-wrapper.py
+#./benchmark.sh gcc -O3 -march=native -fno-tree-vectorize
+#./benchmark.sh python2.7
+#./benchmark.sh python2.6 psyco-wrapper.py
 #./benchmark.sh luajit-2.0.0-beta10
 #./benchmark.sh luajit-2.0.0-beta10 -O-loop
 ./benchmark.sh luajit
