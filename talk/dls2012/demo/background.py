5605a5b68aa8b0db9a33f70c6efb7a7230b3217b
Hakan Ardo <hakan@debian.org>
2012-09-12 09:20:46 +0200

    expose properties and use more descriptive names

--- talk/dls2012/demo/background.py
+++ talk/dls2012/demo/background.py
@@ -2,10 +2,9 @@ from reloader import ReloadHack
 
 class Background(ReloadHack):
     def __init__(self):
-        self.fcnt = self.bkg = 0
+        self.fcnt = self.image = 0
 
-    def update(self, img):
-        self.bkg = (self.fcnt * self.bkg + img) / (self.fcnt + 1)
+    def update(self, frame):
+        self.image = (self.fcnt * self.image + frame) / (self.fcnt + 1)
         self.fcnt += 1
-        return self.bkg
 
