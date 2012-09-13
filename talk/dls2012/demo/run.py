65f0d4e0bc89d9696df42d88372904ffb6515806
Hakan Ardo <hakan@debian.org>
2012-09-13 20:03:29 +0200

    measure fps, pypy is 70 times faster than cpython here

--- talk/dls2012/demo/run.py
+++ talk/dls2012/demo/run.py
@@ -1,16 +1,26 @@
 from io import mplayer, view
 from analytics import Tracker
-import sys
+import sys, time
 
 if len(sys.argv) > 1:
     fn = sys.argv[1]
 else:
     fn = 'demo.avi'
 
+t0 = time.time()
+fcnt = 0
+
 tracker = Tracker()
 while True:
     for img in mplayer(fn):
         view(img, 'Input')
         tracker.update(img)
+
+        t1 = time.time()
+        if t1 - t0 > 1:
+            print >>sys.stderr, '\r%f fps' % (fcnt / (t1-t0)),
+            t0 = t1
+            fcnt = 0
+        fcnt += 1
 
 
