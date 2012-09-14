ddbfa1f1f22f0706b305b5fcbbc4fefc8ff6a2d7
Hakan Ardo <hakan@debian.org>
2012-09-14 11:00:30 +0200

    use PixelIterator in demo as well

--- talk/dls2012/demo/detect.py
+++ talk/dls2012/demo/detect.py
@@ -1,18 +1,7 @@
+
 from reloader import autoreload
 from io import view, viewsc
-
-def morph(fg, r, fn):
-    res = fg.new()
-    for y in xrange(fg.height):
-        for x in xrange(fg.width):
-            #res[x, y] = max(fg[x+dx, y+dy] 
-            #                for dx in xrange(-r, r+1) 
-            #                for dy in xrange(-r, r+1))
-            res[x, y] = fg[x, y]
-            for dx in xrange(-r, r+1):
-                for dy in xrange(-r, r+1):
-                    res[x, y] = fn(res[x, y], fg[x+dx, y+dy])
-    return res
+from image import PixelIter
 
 def morph(fg, r, fn):
     xres = fg.new()
@@ -27,96 +16,15 @@ def morph(fg, r, fn):
             res[x, y] = fn(res[x, y], xres[x, y+dy])
     return res
 
-def erode(fg, r=1):
-    return morph(fg, r, min)
-
 def dilate(fg, r=1):
     return morph(fg, r, max)
 
-class Labler(object):
-    def __init__(self, seg):
-        self.labels = seg.new()
-        self.last_label = 0
-        self.done = False
-
-    def update(self, x, y, neighbours):
-        neighbours = set(neighbours)
-        neighbours.discard(0)
-        if not neighbours:
-            self.last_label += 1
-            l = self.last_label
-        else:
-            l = min(neighbours)
-        if self.labels[x, y] != l:
-            self.done = False
-        self.labels[x, y] = l
-
-    def __getitem__(self, (x, y)):
-        return self.labels[x, y]
-
-    def renumber(self):
-        ll = list(set(self.labels))
-        ll.sort()
-        if ll[0] != 0:
-            ll.insert(0, 0)
-        for x, y in self.labels.indexes():
-            self.labels[x, y] = ll.index(self.labels[x, y])
-        self.last_label = len(ll) - 1
-
-
-def bwlabel(seg):
-    labels = Labler(seg)
-    while not labels.done:
-        labels.done = True
-        for x, y in seg.indexes():
-            if seg[x, y]:
-                ll = [labels[x, y], labels[x-1, y], labels[x-1, y-1], labels[x, y-1], labels[x+1, y-1]]
-                labels.update(x, y, ll)
-
-        for x, y in reversed(seg.indexes()):
-            if seg[x, y]:
-                ll = [labels[x, y], labels[x+1, y], labels[x-1, y+1], labels[x, y+1], labels[x+1, y+1]]
-                labels.update(x, y, ll)
-    labels.renumber()
-    return labels.labels
-
-class BoundingBox(object):
-    maxx = maxy = float('-Inf')
-    minx = miny = float('Inf')
-
-    def add(self, x, y):
-        self.maxx = max(self.maxx, x)
-        self.maxy = max(self.maxy, y)
-        self.minx = min(self.minx, x)
-        self.miny = min(self.miny, y)
-
-    def draw(self, img):
-        for y in xrange(self.miny, self.maxy + 1):
-            img[self.maxx, y] = 255
-            img[self.minx, y] = 255
-        for x in xrange(self.minx, self.maxx + 1):
-            img[x, self.miny] = 255
-            img[x, self.maxy] = 255
-
-    def area(self):
-        return (self.maxx - self.minx + 1) * (self.maxy - self.miny + 1)
-
-def extract_boxes(labels):
-    boxes = {}
-    for x, y in labels.indexes():
-        l = labels[x, y]
-        if l:
-            if l not in boxes:
-                boxes[l] = BoundingBox()
-            boxes[l].add(x, y)
-    return boxes.values()
-
+def erode(fg, r=1):
+    return morph(fg, r, min)
 
 @autoreload
-def find_objects(fg, minarea=100):
+def find_objects(fg):
     seg = erode(dilate(fg, 3), 4)
-    labels = bwlabel(seg)
-    boxes = extract_boxes(labels)
-    boxes = [b for b in boxes if b.area() >= minarea]
-    viewsc(labels, 'segments')
-    return boxes
+    viewsc(seg, 'segments')
+
+
