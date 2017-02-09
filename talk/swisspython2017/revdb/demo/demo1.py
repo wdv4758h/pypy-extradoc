6503d5d538ed1defff1826d587f3c40a4713afb5
Armin Rigo <arigo@tunes.org>
2017-02-09 07:41:58 +0100

    Slides, same as for pyconza2016

---
+++ talk/swisspython2017/revdb/demo/demo1.py
@@ -0,0 +1,19 @@
+
+class X(object):
+    def __init__(self, value):
+        self.value = value
+
+def make_list():
+    lst = [X(5) for i in range(100)]
+    lst.append(lst[50])
+    return lst
+
+lst = make_list()
+
+for x in lst:
+    x.value += 1
+
+for i in range(len(lst)):
+    x = lst[i]
+    x.value += 1
+    assert x.value == 7
